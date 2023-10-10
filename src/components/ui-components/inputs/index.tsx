c668cd1671143c30b5395206235eeb975918a556
Andrew <a.moskalenko@tg.dunice.net>
2023-10-10 14:50:30 +0300

    delete useMemo from btn and input

--- src/components/ui-components/inputs/index.tsx
+++ src/components/ui-components/inputs/index.tsx
@@ -1,4 +1,3 @@
-import { useMemo } from "react";
 import { DefaultInput } from "./default-input";
 import { FileInput } from "./file-input";
 import { MultipleInput } from "./multiple-input";
@@ -7,16 +6,12 @@ export function Input({
   type,
   ...props
 }: React.InputHTMLAttributes<HTMLInputElement>) {
-  const currentInput = useMemo(() => {
-    switch (type) {
-      case "file":
-        return <FileInput {...props} />;
-      case "multiple":
-        return <MultipleInput {...props} />;
-      default:
-        return <DefaultInput {...props} />;
-    }
-  }, [type, props]);
-
-  return currentInput;
+  switch (type) {
+    case "file":
+      return <FileInput {...props} />;
+    case "multiple":
+      return <MultipleInput {...props} />;
+    default:
+      return <DefaultInput {...props} />;
+  }
 }
