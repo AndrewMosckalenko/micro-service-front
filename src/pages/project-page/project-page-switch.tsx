c41ce367a8d28d9676235cfe282a7514fa6e4bf6
Andrew <a.moskalenko@tg.dunice.net>
2023-10-11 12:11:25 +0300

    add projectPageSwitch component

---
+++ src/pages/project-page/project-page-switch.tsx
@@ -0,0 +1,21 @@
+import { lazy } from "react";
+import { useParams } from "react-router-dom";
+
+const SummaryPage = lazy(() => import("../summary-page"));
+const DocumentListPage = lazy(() => import("../document-list-page"));
+const NotFoundPage = lazy(() => import("../not-found-page"));
+
+export function ProjectPageSwitch() {
+
+  const { location } = useParams();
+
+  switch (location) {
+    case "document":
+      return <DocumentListPage />;
+    case "summary":
+      return <SummaryPage />;
+    default:
+      return <NotFoundPage />;
+  }
+  
+}
--- src/pages/project-page/project-page.tsx
+++ src/pages/project-page/project-page.tsx
@@ -1,30 +1,14 @@
-import { useParams } from "react-router-dom";
-import { lazy } from "react";
-
 import { ProjectNavMenu } from "../../components/project-nav-manu";
-import NotFoundPage from "../not-found-page";
+import { ProjectPageSwitch } from "./project-page-switch";
 
-const SummaryPage = lazy(() => import("../summary-page"));
-const DocumentListPage = lazy(() => import("../document-list-page"));
+
 
 export default function ProjectPage() {
-  const { location } = useParams();
-
-  const page = () => {
-    switch (location) {
-      case "document":
-        return <DocumentListPage />;
-      case "summary":
-        return <SummaryPage />;
-      default:
-        return <NotFoundPage />;
-    }
-  };
 
   return (
     <div>
       <ProjectNavMenu />
-      {page()}
+      <ProjectPageSwitch />
     </div>
   );
 }
