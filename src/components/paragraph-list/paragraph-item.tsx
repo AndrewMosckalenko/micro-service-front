62e3c4eb4ba8383d46404a5362ed2096f6f2e07b
Andrew <a.moskalenko@tg.dunice.net>
2023-10-12 10:52:23 +0300

    updates after rebase

--- src/components/paragraph-list/paragraph-item.tsx
+++ src/components/paragraph-list/paragraph-item.tsx
@@ -12,7 +12,7 @@ import { useComponentUpdate } from "../../hooks";
 import { AddIcon, CloseIcon, EditIcon, SaveIcon } from "../svg-icons";
 import { Input } from "../ui-components";
 
-import styles from "./paragraph-list.module.css";
+import styles from "./paragraph-list.module.scss";
 
 export const ParagraphItem = ({
   paragraph,
