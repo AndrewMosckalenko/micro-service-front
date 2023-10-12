67d93e3ac3d4eddad470bd680c2ed5f8c7d17fb9
Andrew <a.moskalenko@tg.dunice.net>
2023-10-12 16:52:19 +0300

    edit table and add tooltip

--- src/components/summary-table/summary-table-cell.tsx
+++ src/components/summary-table/summary-table-cell.tsx
@@ -1,11 +1,29 @@
 import cn from "classnames";
+import { Tooltip } from "react-tooltip";
+
 import styles from "./summary-table.module.scss";
+import { useMemo } from "react";
 
 export function SummaryTableCell({
   children,
   header,
   left,
 }: ISummaryTableCellProps) {
+  const { isShort, value } = useMemo(() => {
+    if (typeof children === "string" && header) {
+      const isShort = children.length > 5;
+      return {
+        isShort,
+        value: `${children.slice(0, 5)}${isShort ? "..." : ""}`,
+      };
+    } else {
+      return {
+        isShort: false,
+        value: children,
+      };
+    }
+  }, [children, header]);
+
   return (
     <td
       className={cn(styles.summary_table__cell, {
@@ -13,7 +31,21 @@ export function SummaryTableCell({
         [styles.summary_table__left_cell]: left,
       })}
     >
-      <p>{children}</p>
+      <div
+        className={cn(styles.summary_table__content_wrapper, {
+          [styles.summary_table__left_content_wrapper]: left,
+        })}
+      >
+        <a
+          data-tooltip-id="table_cell_tooltip"
+          data-tooltip-content={children}
+          data-tooltip-place="top"
+          className={styles.summary_table__content}
+        >
+          {value}
+        </a>
+      </div>
+      {isShort && <Tooltip id="table_cell_tooltip" />}
     </td>
   );
 }
