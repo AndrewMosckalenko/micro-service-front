6bbaa4294d3c621d49522ac2a56933f7f4a9f2f2
Ne Andrew <a.moskalenko@tg.dunice.net>
2023-11-02 12:50:31 +0300

    make sort global helper

--- src/components/summary-table/summary-table-row.tsx
+++ src/components/summary-table/summary-table-row.tsx
@@ -1,4 +1,4 @@
-import { sortTags } from "../../helpers/sort-tags";
+import { sortCopy } from "../../helpers/sort-copy";
 import { ISummaryCell, ISummaryRow } from "../../interfaces";
 
 import { SummaryTableCell } from "./summary-table-cell";
@@ -6,7 +6,7 @@ import { SummaryTableCell } from "./summary-table-cell";
 import styles from "./summary-table.module.scss";
 
 export function SummaryTableRow({ row }: ISummaryTableRowProps) {
-  const tagList = sortTags(row);
+  const tagList = sortCopy(row.tags, (a: ISummaryCell, b: ISummaryCell) => a.tag.id - b.tag.id);
 
   return (
     <tr className={styles.summaryTableRow}>
--- src/helpers/sort-tags.ts
+++
@@ -1,6 +0,0 @@
-import { ISummaryCell, ISummaryRow } from "../interfaces";
-
-export const sortTags = (row: ISummaryRow) =>
-  row.tags
-    .slice(0)
-    .sort((a: ISummaryCell, b: ISummaryCell) => a.tag.id - b.tag.id);
